e70ad47bf46b85efd188d685c9a439bea42a6c20
Tony Frieiro <tonyfrieiro@gmail.com>
2022-08-08 21:40:23 -0300

    incorporar eventos

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ function Producto (nombre, precio ,){
     this.precio = precio;
 }
 
-let pagarTotal = 0;
-
-function total(precio){
-    (parseFloat(precio))
-    pagarTotal = pagarTotal + precio
+function total2(){
+    let verTotal = document.getElementById("botonTotal")
+    verTotal.innerText = "$ "+ acumulador+""
+    console.log("total: "+acumulador)
 }
 
 const arrayCarrito = [];
+
+let acumulador = 0;
 
 const botonTote = document.getElementById("botonTote")
 
@@ -21,16 +22,18 @@ function añadido(nombre){
 botonTote.addEventListener("click",()=>{
     let nombre = "ToteNegra";
     let precio = 2000;
+    acumulador = acumulador + precio
 
-    const prodcuto1 = new Producto (nombre, precio);
+    const producto1 = new Producto (nombre, precio);
 
-    arrayCarrito.push(prodcuto1)
+    arrayCarrito.push(producto1)
 
     añadido()
 
     console.log(arrayCarrito)
 
-    total();
+    total2()
+
 })
 
 const botonBuzo = document.getElementById("botonBuzo")
@@ -38,16 +41,18 @@ const botonBuzo = document.getElementById("botonBuzo")
 botonBuzo.addEventListener("click",()=>{
     let nombre = "Buzo Bordado";
     let precio = 4000;
+    acumulador = acumulador + precio
 
-    const prodcuto1 = new Producto (nombre, precio);
+    const producto1 = new Producto (nombre, precio);
 
-    arrayCarrito.push(prodcuto1)
+    arrayCarrito.push(producto1)
 
     añadido()
 
     console.log(arrayCarrito)
 
-    total();
+    total2()
+
 })
 
 const botonRemera = document.getElementById("botonRemera")
@@ -55,16 +60,18 @@ const botonRemera = document.getElementById("botonRemera")
 botonRemera.addEventListener("click",()=>{
     let nombre = "Remera Bordada";
     let precio = 2000;
+    acumulador = acumulador + precio
 
-    const prodcuto1 = new Producto (nombre, precio);
+    const producto1 = new Producto (nombre, precio);
 
-    arrayCarrito.push(prodcuto1)
+    arrayCarrito.push(producto1)
 
     añadido()
 
     console.log(arrayCarrito)
 
-    total();
+    total2()
+
 })
 
 const botonCartuchera = document.getElementById("botonCartuchera")
@@ -72,16 +79,18 @@ const botonCartuchera = document.getElementById("botonCartuchera")
 botonCartuchera.addEventListener("click",()=>{
     let nombre = "Cartuchera bordada";
     let precio = 2500;
+    acumulador = acumulador + precio
 
-    const prodcuto1 = new Producto (nombre, precio);
+    const producto1 = new Producto (nombre, precio);
 
-    arrayCarrito.push(prodcuto1)
+    arrayCarrito.push(producto1)
 
     añadido()
 
     console.log(arrayCarrito)
 
-    total();
+    total2()
+
 })
 
 const botonCuadro = document.getElementById("botonCuadro")
@@ -89,23 +98,26 @@ const botonCuadro = document.getElementById("botonCuadro")
 botonCuadro.addEventListener("click",()=>{
     let nombre = "Cuadro Bordado";
     let precio = 1700;
+    acumulador = acumulador + precio
 
-    const prodcuto1 = new Producto (nombre, precio);
+    const producto1 = new Producto (nombre, precio);
 
-    arrayCarrito.push(prodcuto1)
+    arrayCarrito.push(producto1)
 
     añadido()
 
     console.log(arrayCarrito)
 
-    total();
+    total2()
 
 })
 
 const botonCarrito = document.getElementById("botonCarrito")
 
 botonCarrito.addEventListener("click",()=>{
-    let verTotal = document.getElementById("botonTotal")
-    console.log(verTotal.innerText)
-    verTotal.innerText = "$ "+ pagarTotal+""
+    // let verTotal = document.getElementById("botonTotal")
+    // verTotal.innerText = "$ "+ acumulador+""
+    // console.log("total: "+acumulador)
+
 })
+
