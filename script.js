24c21fbbff8d6642e7b6f35e222bff18d19e2209
Tony Frieiro <tonyfrieiro@gmail.com>
2022-08-12 22:15:15 -0300

    segunda entrega proyecto final

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
-function Producto (nombre, precio ,){
-    this.nombre = nombre;
-    this.precio = precio;
+
+// class Producto{
+//     constructor(id,nombre, precio ,img){
+//         this.id = id;
+//         this.nombre = nombre;
+//         this.precio = precio;
+//         this.img = img;
+//     }
+// }
+
+// let botones =[]
+
+// let carrito =[]
+
+// const producto1 = new Producto (1,"ToteNegra",2000,"/img/totenegra.webp")
+// const producto2 = new Producto (2,"BuzoBordado", 4000, "/img/buzoBordado.webp")
+// const producto3 = new Producto (3,"RemeraBordada",2200,"/img/remeraBordada.webp")
+// const producto4 = new Producto (4,"Cartuchera", 2500, "/img/cartuchera.webp")
+// const producto5 = new Producto (5,"CuadroBordado", 1700,"/img/cuadroBordado.webp")
+
+// const productos = [producto5,producto2,producto3,producto4,producto1]
+
+// const contenedorArray = document.getElementById("contenedorArray")
+
+// productos.forEach((producto) => {
+//     contenedorArray.innerHTML += `
+//         <div class="card">
+//             <img class="card-img-top" src="${producto.img}" alt="Card image cap">
+//             <div class="card-body">
+//                 <h5 class="card-title">${producto.nombre}</h5>
+//                 <p class="card-text">${producto.precio}</p>
+//                 <a href="#" class="btn btn-primary" id="agregar${producto.id}" >Comprar</a>
+//             </div>
+//         </div>
+    
+//     `
+//     let boton = document.getElementById(`agregar${producto.id}`)
+
+//     botones.push(boton)
+
+
+//     botones.forEach((boton)=>{
+//         boton.addEventListener('click', () => {
+//             //esta funcion ejecuta el agregar el carrito con la id del producto
+//             agregarAlCarrito(producto.id)
+//             //
+//         })
+//     })
+
+// })
+
+// const agregarAlCarrito = (prodId) => {
+//     const item = productos.find((prod) => prod.id === prodId)
+//     carrito.push(item)
+//     console.log(carrito)
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Producto{
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
 
 function total2(){
@@ -11,12 +72,27 @@ function total2(){
 
 const arrayCarrito = [];
 
+var valor = 0;
+
 let acumulador = 0;
 
 const botonTote = document.getElementById("botonTote")
 
-function añadido(nombre){
-    alert(" "+ nombre +" a sido añadido al carrito")
+function añadido(){
+    Toastify({
+        text: "Producto añadido",
+        duration: 3000,
+        newWindow: false,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #cbc571, #9e9c7d)",
+          color: "#000000"
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
 }
 
 botonTote.addEventListener("click",()=>{
@@ -33,6 +109,13 @@ botonTote.addEventListener("click",()=>{
     console.log(arrayCarrito)
 
     total2()
+
+    //const enJSON = JSON.stringify(producto1);
+    //localStorage.setItem(valor, enJSON);
+
+    valor = valor + 1
+
+    localStorage.setItem(valor, JSON.stringify(producto1));
 
 })
 
@@ -53,6 +136,11 @@ botonBuzo.addEventListener("click",()=>{
 
     total2()
 
+    //const enJSON = JSON.stringify(producto1);
+    //localStorage.setItem(valor, enJSON);
+    valor = valor + 1
+
+    localStorage.setItem(valor, JSON.stringify(producto1));
 })
 
 const botonRemera = document.getElementById("botonRemera")
@@ -72,6 +160,11 @@ botonRemera.addEventListener("click",()=>{
 
     total2()
 
+    //const enJSON = JSON.stringify(producto1);
+    //localStorage.setItem(valor, enJSON);
+    valor = valor + 1
+
+    localStorage.setItem(valor, JSON.stringify(producto1));
 })
 
 const botonCartuchera = document.getElementById("botonCartuchera")
@@ -91,6 +184,11 @@ botonCartuchera.addEventListener("click",()=>{
 
     total2()
 
+    //const enJSON = JSON.stringify(producto1);
+    //localStorage.setItem(valor, enJSON);
+    valor = valor + 1
+
+    localStorage.setItem(valor, JSON.stringify(producto1));
 })
 
 const botonCuadro = document.getElementById("botonCuadro")
@@ -110,6 +208,11 @@ botonCuadro.addEventListener("click",()=>{
 
     total2()
 
+    //const enJSON = JSON.stringify(producto1);
+    //localStorage.setItem(valor, enJSON);
+    valor = valor + 1
+
+    localStorage.setItem(valor, JSON.stringify(producto1));
 })
 
 const botonCarrito = document.getElementById("botonCarrito")
@@ -118,6 +221,21 @@ botonCarrito.addEventListener("click",()=>{
     // let verTotal = document.getElementById("botonTotal")
     // verTotal.innerText = "$ "+ acumulador+""
     // console.log("total: "+acumulador)
+    localStorage.clear()
+    acumulador = 0
+    total2()
+    Toastify({
+        text: "Cariito vaciado",
+        duration: 3000,
+        newWindow: false,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #862222, #191828)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
 
 })
-
